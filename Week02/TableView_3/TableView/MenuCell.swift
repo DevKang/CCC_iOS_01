d1172b6c48a53d81e04d08773a5b08a55a6f3863
hs <gytjdslazoq@gmail.com>
2020-06-29 16:18:54 +0900

    update

---
+++ Week02/TableView_3/TableView/MenuCell.swift
@@ -0,0 +1,24 @@
+//
+//  MenuCell.swift
+//  TableView
+//
+//  Created by vlm on 2020/04/16.
+//  Copyright © 2020 vlm. All rights reserved.
+//
+
+import UIKit
+
+class MenuCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
